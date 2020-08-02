163d470501780c45f2e1d8d5b0e6741f0b973790
Vladislav Soprun <develop@soprun.com>
2020-08-02 13:11:39 +0300

    commit...

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,28 @@
 #!/bin/sh
 
-set -ex
+set -e
+
+# export APP_SECRET=$(openssl rand -hex 16);
+# export APP_RELEASE=$(git rev-parse HEAD);
+# echo "secret: ${APP_SECRET}"
+# echo "release: ${APP_RELEASE}"
 
 docker build \
 	--file ./docker/nginx/Dockerfile \
 	--tag sandbox/nginx \
 	.
+
+# todo: Set label when building image.
+#LABEL com.inspigate.app.type="php"
+#LABEL com.inspigate.app.debug="false"
+#LABEL com.inspigate.app.name="billing"
+#LABEL com.inspigate.app.description=""
+#LABEL com.inspigate.server.ssl=""
+#LABEL com.inspigate.server.external_port=""
+##LABEL com.inspigate.version="${APP_VERSION}"
+##LABEL com.inspigate.release="${APP_RELEASE}"
+#LABEL com.inspigate.stage="builder"
+#LABEL com.inspigate.description="..."
 
 docker build \
 	--build-arg app_env=dev \
--- docker/php-fpm/init.sh
+++ docker/php-fpm/init.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
+set -e
+
 # first arg is `-f` or `--some-option`
 if [[ "${1#-}" != "$1" ]]; then
 	set -- php "$@"
 fi
-
-set -o errexit
-set -o nounset
 
 echo 'Linting PHP files'
 
@@ -14,8 +13,20 @@ if ! composer --version > /dev/null 2> /dev/null; then
     printf '\e[0;31m%-6s\e[m\n\n' 'Composer is not installed.'
 fi
 
+if ! composer global show hirak/prestissimo > /dev/null 2> /dev/null; then
+  printf '\e[0;31m%-6s\e[m\n\n' 'Composer global package "hirak/prestissimo" is not installed.'
+  composer global require hirak/prestissimo --quiet;
+fi
+
 if ! test -f ./vendor/autoload.php; then
     printf '\e[0;31m%-6s\e[m\n\n' 'Composer dependencies are not installed.'
+fi
+
+# Use the default production configuration
+mv "$PHP_INI_DIR/php.ini-production" "$PHP_INI_DIR/php.ini"
+
+if [ ${APP_ENV} = "dev" ]; then \
+  mv "$PHP_INI_DIR/php.ini-development" "$PHP_INI_DIR/php.ini"
 fi
 
 php-fpm --nodaemonize
