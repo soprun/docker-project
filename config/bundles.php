8eaf65788d1325146722090b10556f18d410720c
Vladislav Soprun <develop@soprun.com>
2020-08-22 17:49:22 +0300

    commit...

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,5 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
 ];
