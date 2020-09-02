ee56515bb8d4e9f96365ce626a3a63d07d66c4ba
Vladislav Soprun <develop@soprun.com>
2020-09-03 01:46:57 +0300

    commit...

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
-clear -x
+
+clear
+tput sgr0
 
 # echo "$(date "+%Y.%m.%d %H:%M:%S")"
 # 2020.09.03 00:34:42
@@ -11,34 +13,33 @@ log() {
 }
 
 success() {
-  echo "=>\033[0;32m log:\033[0m $* "
+  printf "=>\033[0;32m log: \033[0m%-6s\n" "$*"
   logger -p user.info -t "$(basename "${0}")" "$@"
   sleep .3
 }
 
 info() {
-  echo "=>\033[0;34m log:\033[0m $* "
+  printf "=>\033[0;34m log: \033[0m%-6s\n" "$*"
   logger -p user.info -t "$(basename "${0}")" "$@"
   sleep .3
 }
 
 warn() {
-  echo "=>\033[0;33m log.warn:\033[0m $* "
+  printf "=>\033[0;33m warn: \033[0m%-6s\n" "$*"
   logger -p user.warn -t "$(basename "${0}")" "$@"
 }
 
 error() {
-  echo "=>\033[0;31m log.error:\033[0m $* " >&2
+  printf "=>\033[0;31m error: \033[0m%-6s\n" "$*" >&2
   logger -p user.error -t "$(basename "${0}")" "$@"
   exit 1
 }
 
+# see: https://apple.stackexchange.com/questions/256769/how-to-use-logger-command-on-sierra
 #log 'log'
 #success 'success'
 #info 'info'
 #error 'error'
-
-# https://apple.stackexchange.com/questions/256769/how-to-use-logger-command-on-sierra
 
 # log show --style compact --info --debug --predicate 'process == "logger"' --last 20m
 # log stream --process logger --level debug --style syslog
@@ -65,8 +66,6 @@ if ! openssl version >/dev/null 2>/dev/null; then
   error 'Command "openssl" is not installed.'
 fi
 
-success 'Check system required dependencies - is succeeded!'
-
 if ! shellcheck --version >/dev/null 2>/dev/null; then
   error 'Command "shellcheck" is not installed.'
 fi
@@ -77,66 +76,15 @@ success 'Check system required dependencies - is succeeded!'
 ### Globals
 ###
 
-#APP_DOCUMENT_ROOT="$(dirname "$(readlink "$BASH_SOURCE")")"
-#export APP_DOCUMENT_ROOT
-
 info 'Load environment variables'
 
 # 1. Check if .env file exists
 if [ -e .env ]; then
-  # set -o allexport
   # shellcheck source=./.env
   source .env
-  # set +o allexport
-fi
-
-# 2. Check if .env.local file exists
-if [ -r .env.local ]; then
-  # set -o allexport
-  # shellcheck source=./.env.local
-  source .env.local
-  # set +o allexport
 fi
 
 success 'Load environment variables - is succeeded!'
-
-###
-### Create default network
-###
-
-info 'Check network'
-
-if ! docker network inspect "${NETWORK_NAME}" >/dev/null 2>/dev/null; then
-  warn "Network ${NETWORK_NAME} is not create."
-  docker network create "${NETWORK_OPTIONS}" "${NETWORK_NAME}"
-fi
-
-success 'Check network - is succeeded!'
-
-###
-### SSL certificate
-###
-
-info 'Check SSL certificate'
-
-readonly nginx_ssl_dir="./docker/nginx/ssl"
-
-if [ ! -e "$nginx_ssl_dir/key.pem" ]; then
-  log_info 'Create locally-trusted development certificate.'
-
-  mkcert \
-    -key-file "$nginx_ssl_dir/key.pem" \
-    -cert-file "$nginx_ssl_dir/cert.pem" \
-    "$APP_HOST" "*.$APP_HOST" localhost 127.0.0.1 ::1
-fi
-
-if [ ! -e "$nginx_ssl_dir/dhparam.pem" ]; then
-  log_info "Create Diffie-Hellman 🔐"
-
-  openssl dhparam -out "$nginx_ssl_dir/dhparam.pem" 1024 >/dev/null 2>&1
-fi
-
-success 'Check SSL certificate - is succeeded!'
 
 ###
 ### Application environment variables
@@ -180,10 +128,50 @@ export GIT_COMMIT_SHA=${GIT_COMMIT_SHA}
 
 success 'Check application environment variables - is succeeded!'
 log "$(printenv | sort | less)"
+set -u
+
+###
+### Create default network
+###
+
+info 'Check network'
+
+if ! docker network inspect "${NETWORK_NAME}" >/dev/null 2>/dev/null; then
+  warn "Network ${NETWORK_NAME} is not create."
+  docker network create "${NETWORK_OPTIONS}" "${NETWORK_NAME}"
+fi
+
+success 'Check network - is succeeded!'
+
+###
+### SSL certificate
+###
+
+info 'Check SSL certificate'
+
+readonly nginx_ssl_dir="./docker/nginx/ssl"
+
+if [ ! -e "$nginx_ssl_dir/key.pem" ]; then
+  log_info 'Create locally-trusted development certificate.'
+
+  mkcert \
+    -key-file "$nginx_ssl_dir/key.pem" \
+    -cert-file "$nginx_ssl_dir/cert.pem" \
+    "$APP_HOST" "*.$APP_HOST" localhost 127.0.0.1 ::1
+fi
+
+if [ ! -e "$nginx_ssl_dir/dhparam.pem" ]; then
+  log_info "Create Diffie-Hellman 🔐"
+
+  openssl dhparam -out "$nginx_ssl_dir/dhparam.pem" 1024 >/dev/null 2>&1
+fi
+
+success 'Check SSL certificate - is succeeded!'
 
 info "Docker running building contractors! 🐳 "
 
 docker build \
+  --squash \
   --build-arg APP_ENV="${APP_ENV}" \
   --build-arg APP_DEBUG="${APP_DEBUG}" \
   --file ./docker/nginx/Dockerfile \
@@ -195,6 +183,7 @@ docker build \
 success 'Build sandbox-nginx - is succeeded!'
 
 docker build \
+  --squash \
   --build-arg APP_ENV="${APP_ENV}" \
   --build-arg APP_DEBUG="${APP_DEBUG}" \
   --file ./docker/php-cli/Dockerfile \
@@ -206,6 +195,7 @@ docker build \
 success 'Build sandbox-php-cli - is succeeded!'
 
 docker build \
+  --squash \
   --build-arg APP_ENV="${APP_ENV}" \
   --build-arg APP_DEBUG="${APP_DEBUG}" \
   --file ./docker/php/Dockerfile \
@@ -225,9 +215,9 @@ docker-compose --log-level info up \
 
 info '=> Docker push images: 🐳 '
 
-#docker push soprun/sandbox-nginx
-#docker push soprun/sandbox-php
-#docker push soprun/sandbox-php-cli
+docker push soprun/sandbox-nginx
+docker push soprun/sandbox-php
+docker push soprun/sandbox-php-cli
 
 # docker exec -ti php sh
 
