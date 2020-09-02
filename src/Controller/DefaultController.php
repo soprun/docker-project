e109600265cfe55cbb0bd56aadbd4bf0e9d682e3
Vladislav Soprun <develop@soprun.com>
2020-09-02 16:37:35 +0300

    commit...

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -11,6 +11,21 @@ final class DefaultController extends AbstractController
 {
     public function index(): Response
     {
+        try {
+            $connection = new PDO(
+                "pgsql:host=${_ENV['POSTGRES_HOST']};port=${_ENV['POSTGRES_PORT']};dbname=${_ENV['POSTGRES_DB']}",
+                $_ENV['POSTGRES_USERNAME'],
+                $_ENV['POSTGRES_PASSWORD']
+            );
+
+            if ($connection) {
+                echo 'Connection success!'.PHP_EOL;
+                exit(0);
+            }
+        } catch (Throwable $exception) {
+            echo $exception->getMessage();
+        }
+
         return new Response('... : ' . time());
     }
 }
