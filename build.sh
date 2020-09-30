d544c122f2c8a8d86fe0ba54f854ce9dafe1f2fe
Vladislav Soprun <develop@soprun.com>
2020-10-01 01:38:17 +0300

    commit...

--- build.sh
+++ build.sh
@@ -1,6 +1,47 @@
 #!/bin/bash
 
+## Strip git ref prefix from version
+#VERSION=$(echo "${{ github.ref }}" | sed -e 's,.*/\(.*\),\1,')
+#
+## Strip "v" prefix from tag name
+#[[ "${{ github.ref }}" == "refs/tags/"* ]] && VERSION=$(echo $VERSION | sed -e 's/^v//')
+#
+## Use Docker `latest` tag convention
+#[ "$VERSION" == "master" ] && VERSION=latest
+#
+
+VERSION=$(echo $(git describe --tags --abbrev=0) | sed -e 's,.*/\(.*\),\1,')
+
+
+VERSION=$(echo $VERSION | sed -e 's/^v//')
+
+echo $VERSION
+
+headname=develop
+
+git show-ref --quiet --verify -- "refs/heads/$headname" ||
+		echo "$headname is not a valid branch"
+
+exit 1;
+# Return git current tag
+
+# Strip git ref prefix from version
+VERSION=$(echo $(git describe --tags --abbrev=0) | sed -e 's,.*/\(.*\),\1,')
+
+# Strip "v" prefix from tag name
+[[ "$(git describe --tags --abbrev=0)" == "refs/tags/"* ]] && VERSION=$(echo $VERSION | sed -e 's/^v//')
+
+echo $VERSION
+
+exit 0
 readonly name="sandbox-php"
+
+tags=(dev prod)
+
+latest
+
+tags
+
 # readonly tag_latest="latest"
 # readonly tag_builder="builder"
 readonly tag_development="dev"
