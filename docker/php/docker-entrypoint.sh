97a4ded6abd7340040320c4a825aecb496868f45
Vladislav Soprun <develop@soprun.com>
2020-11-08 02:52:10 +0300

    commit...

--- docker/php/docker-entrypoint.sh
+++ docker/php/docker-entrypoint.sh
@@ -2,5 +2,29 @@
 
 set -e
 
-# Then exec the container's main process (what's set as CMD in the Dockerfile).
-exec "$@"
+# first arg is `-f` or `--some-option`
+if [ "${1#-}" != "$1" ]; then
+  set -- php-fpm "$@"
+fi
+
+log_info() {
+  printf "\033[0;34m[info]\033[0m: %s\\n" "$1"
+}
+
+log_success() {
+  printf "\033[0;32m[success]\033[0m: %s\\n" "$1"
+}
+
+log_error() {
+  printf "\033[0;31m[error]\033[0m: %s\\n" "$1"
+}
+
+log_success 'Checking of environment variables. 🗂.'
+
+log_info $APP_ENV
+log_info $APP_DEBUG
+log_info $APP_HOST
+log_info $APP_SCHEME
+log_info $APP_SECURE
+
+exec docker-php-entrypoint "$@"
