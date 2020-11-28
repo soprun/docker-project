ee351a62303aa2d8764f91dcf64cef26af8acae9
Vladislav Soprun <develop@soprun.com>
2020-11-28 21:36:37 +0300

    Signing commit 2020-11-28

--- docker/build.sh
+++ docker/build.sh
@@ -1,15 +1,45 @@
 #!/bin/bash
 
-#!/bin/bash
+set -euo pipefail
 
-set -e
+function output
+{
+  local style_start=""
+  local style_end=""
+  if [ "${2:-}" != "" ]; then
+    case $2 in
+    "success")
+      style_start="\033[0;32m"
+      style_end="\033[0m"
+      ;;
+    "error")
+      style_start="\033[31;31m"
+      style_end="\033[0m"
+      ;;
+    "info" | "warning")
+      style_start="\033[33m"
+      style_end="\033[39m"
+      ;;
+    "heading")
+      style_start="\033[1;33m"
+      style_end="\033[22;39m"
+      ;;
+    esac
+  fi
 
-log_info() {
-  printf "\033[0;34m[info]\033[0m: %s\\n" "$1"
+  builtin echo -e "${style_start}${1}${style_end}"
 }
 
 # Project directory
 PROJECT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
+# Run environment checks.
+output "\nEnvironment check" "heading"
+output "\n${PROJECT_DIR}" "info"
+
+ls -la $PROJECT_DIR
+
+exit 0
 
 # shellcheck source=./.env
 source "${PROJECT_DIR}/../.env"
